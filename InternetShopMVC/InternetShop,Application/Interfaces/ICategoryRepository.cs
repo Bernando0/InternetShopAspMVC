99ab1d60663ba67151400ab5f25dcd7e162c1db7
itStart <itStart@207-4>
2023-04-24 21:52:34 +0600

    Added new project

---
+++ InternetShopMVC/InternetShop,Application/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using InternetShop.Domain.Entities;
+
+namespace InternetShop_Application.Interfaces
+{
+    public interface ICategoryRepository
+    {
+
+
+        public void Add(Category category) {
+
+        }
+    }
+}
---
+++ InternetShopMVC/InternetShop.Presistance/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using InternetShop.Domain.Entities;
+using InternetShop_Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop.Presistance.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository( AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Add(Category category)
+        {
+            _appDbContext.Categories.Add(category);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
